212194622a344cd2cb37ceb3cf911e4bdf76492c
Misery <savvarobotics@icloud.com>
2025-09-22 16:24:18 +0200

    Initial commit 2

---
+++ sine_wave_functions/script.py
@@ -0,0 +1,8 @@
+from sine_wave import create_sine_wave
+import matplotlib.pyplot as plt
+
+sine_wave_1 = create_sine_wave(440, 2)
+sine_wave_2 = create_sine_wave(220, 2)
+sine_wave_3 = create_sine_wave(110, 2)
+
+plt.show()
