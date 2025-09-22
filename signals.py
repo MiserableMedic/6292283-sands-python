a2a1fbc2451c25164514622d200b1471de566078
Misery <savvarobotics@icloud.com>
2025-09-22 17:24:39 +0200

    added signals file

--- signals.py
+++ signals.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 def create_sine_wave(frequency, duration, sample_rate):
-    t = np.linspace(0, duration, sample_rate)
+
+    t = np.linspace(0, duration, sample_rate * duration)
     sine_wave = np.sin(2 * np.pi * frequency * t)
-    return sine_wave
+
+    return t, sine_wave
