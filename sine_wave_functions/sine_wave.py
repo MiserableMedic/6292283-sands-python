0982b910624ae7ec02cd56b46824d6d48bbfe8fd
Misery <savvarobotics@icloud.com>
2025-10-04 01:44:15 +0800

    removed pestering useless file

--- sine_wave_functions/sine_wave.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-def create_sine_wave(frequency, duration):
-    t = np.linspace(0, duration)
-    sine_wave = np.sin(2 * np.pi * frequency * t)
-    plt.plot(sine_wave, label=f'Frequency: {frequency}')
