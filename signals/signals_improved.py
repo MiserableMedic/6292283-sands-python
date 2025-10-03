78c377234bbb522e7e035f95477094d6fa289a05
Misery <savvarobotics@icloud.com>
2025-10-04 03:13:24 +0800

    all original functions from gen_signals rewritten in class format. Additional add, multiply and shift functions have been added in the Signal class

--- signals/signals_improved.py
+++ signals/signals_improved.py
@@ -7,11 +7,19 @@ class Signal:
         self.samples = samples
         self.sample_rate = sample_rate
 
+    def add(self, other):
+        return Signal(self.t, self.samples + other.samples, self.sample_rate)
+
+    def multiply(self, other):
+        return Signal(self.t, self.samples * other.samples, self.sample_rate)
+
+    def shift(self, displace: float):
+        return Signal(self.t + displace, self.samples, self.sample_rate)
+
     def plot(self):
         plt.plot(self.t, self.samples)
         plt.xlabel('Time [s]')
         plt.ylabel('Amplitude')
-        plt.title('Signal')
         plt.grid()
         plt.show()
 
@@ -35,3 +43,26 @@ class GenSignal:
         samples = amp * np.sin(2*np.pi*freq*t + phase)
 
         return Signal(t, samples, self.sample_rate)
+    
+    def cosine(self, freq, duration, amp=1.0, phase=0.0):
+        return self.sine(freq, duration, amp=amp, phase=(np.pi/2 + phase))
+    
+    def sinc(self, duration, amp=1.0, phase=0.0):
+        t = self._time(duration)
+        samples = amp * np.sinc(t + phase)
+
+        return Signal(t, samples, self.sample_rate)
+    
+    def unit_step(self, duration, amp=1.0, displace=0.0):
+        t = self._time(duration)
+        samples = np.where(t < 0 - displace, 0, amp)
+
+        return Signal(t, samples, self.sample_rate)
+    
+    def pulse(self, duration, amp=1.0, displace=0.0):
+        step_up = self.unit_step(duration, amp=1.0, displace=(0.5 - displace))
+        step_down = self.unit_step(duration, amp=-1.0, displace=(-0.5 - displace))
+
+        samples = amp * (step_up.samples + step_down.samples)
+
+        return Signal(step_up.t, samples, self.sample_rate)
