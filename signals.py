e98e78c6dac752d3afd04d9c871fc834835b6c6e
Misery <savvarobotics@icloud.com>
2025-10-03 14:23:03 +0800

    added cosine function

--- signals.py
+++ signals.py
@@ -6,3 +6,10 @@ def create_sine_wave(frequency, duration, sample_rate):
     sine_wave = np.sin(2 * np.pi * frequency * t)
 
     return t, sine_wave
+
+def create_cosine_wave(frequency, duration, sample_rate):
+
+    t = np.linspace(0, duration, sample_rate * duration)
+    cosine_wave = np.cos(2 * np.pi * frequency * t)
+
+    return t, cosine_wave
